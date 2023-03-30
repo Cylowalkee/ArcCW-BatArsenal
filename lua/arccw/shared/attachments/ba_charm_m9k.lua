b09736997834b59ae1aa17b60c37940888e65614
Cylowalker <76599609+Cylowalkee@users.noreply.github.com>
2023-03-30 12:59:21 +0300

    get cool

---
+++ lua/arccw/shared/attachments/ba_charm_m9k.lua
@@ -0,0 +1,22 @@
+att.PrintName = "Murderthon 9000"
+att.Icon = Material("entities/att/acwatt_ba_charm_m9k.png")
+att.Description = "The icon of a Garry's Mod addon. M9K Unrealism FTW!!"
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "charm"
+
+att.Free = true
+
+att.Model = "models/weapons/arccw/atts/charmbase.mdl"
+
+att.DroppedModel = "models/Items/BoxSRounds.mdl"
+
+att.Charm = true
+att.CharmModel = "models/weapons/arccw/atts/ba_m9kcharm.mdl"
+att.CharmAtt = "Charm"
+att.CharmScale = Vector(2, 2, 2)
+att.CharmOffset = Vector(0, 0, 0)
+att.CharmAngle = Angle(20, 0, 80)
