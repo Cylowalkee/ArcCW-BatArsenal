ce831ee5115aa29d6c8a623af68f8e60c37f1cfa
Cylowalker <76599609+Cylowalkee@users.noreply.github.com>
2024-02-14 23:38:15 +0300

    Yeah

---
+++ lua/arccw/shared/attachments/ba_foregrip_angled.lua
@@ -0,0 +1,23 @@
+att.PrintName = "Angled Foregrip"
+att.Icon = Material("entities/att/acwatt_ba_foregrip_kac.png")
+att.Description = "An angled foregrip that improves ergonomics."
+
+att.SortOrder = 1
+
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "foregrip"
+
+att.LHIK = true
+
+att.Model = "models/weapons/arccw/atts/ba_fg_angled.mdl"
+att.ModelOffset = Vector(1.5, 0, 0)
+
+att.Mult_RecoilSide = 1.1
+
+att.Mult_SightTime = 0.9
+
+att.Mult_SpeedMult = 1.05
